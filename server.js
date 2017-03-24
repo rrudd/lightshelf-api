1c8ff364392098209d01ea8a78901fb34a28cc6d
Marika Mörsky <marika.morsky@netlight.com>
2017-03-24 09:55:47 +0200

    Fix a method depricated error in server.js: use sendStatus method instead of send. Bug fix in borrowing a book: we need to populate the book object with the related loan object since the client is expecting the loan to be an object instead of id reference

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT");
   if(req.method === 'OPTIONS') {
-      res.send(200);
+      res.sendStatus(200);
       return;
   }
   next();
